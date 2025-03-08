e685ad105a01b1efc0869327640e19481cb27f9f
dieperid <david.dieperink@ik.me>
2025-03-08 18:21:49 +0100

    test: implemented mock of i18next and created 3 test case for the 3 others scenario

--- test/jest/translation.test.js
+++ test/jest/translation.test.js
@@ -1,13 +1,36 @@
 "use strict";
 
-//let CartItem = require('../src/CartItem/CartItem.js');
+const i18next = jest.fn().mockImplementation((lng, fallbackLng, resources) => {
+    return {
+        lng,
+        fallbackLng,
+        resources,
+        addResourceBundle: jest.fn(),
+        changeLanguage: jest.fn(),
+    };
+});
 
-
-test('translation_NominalLanguage_NavigatorLanguageKnow', () => {
+test("translation_NominalLanguage_NavigatorLanguageKnow", () => {
     //given
-
     //when
     //we call the getters directly in assertion below
-
     //then
-})
+});
+
+test("translation_NominalLanguage_ChangeNavigatorLanguage", () => {
+    //given
+    //when
+    //then
+});
+
+test("translation_ChangeNavigatorLanguage_ThrowException", () => {
+    //given
+    //when
+    //then
+});
+
+test("translation_ChangeNavigatorLanguage_PageIsTranslated", () => {
+    //given
+    //when
+    //then
+});
