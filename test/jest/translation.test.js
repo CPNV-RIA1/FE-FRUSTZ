75523aac6d16c85a91e3f3063aa83bb3d8442239
dieperid <david.dieperink@ik.me>
2025-03-11 11:45:11 +0100

    test: implemented Jest unit test for scenario 002

--- test/jest/translation.test.js
+++ test/jest/translation.test.js
@@ -12,10 +12,19 @@ describe("Scenario 001 - Langue par défaut", () => {
     });
 });
 
-test("translation_NominalLanguage_ChangeNavigatorLanguage", () => {
-    //given
-    //when
-    //then
+describe("Scénario 002 - Changement de langue du navigateur", () => {
+    test("translation_NominalLanguage_ChangeNavigatorLanguage", () => {
+        // Given :
+        // La page d'accueil est chargée.
+        // La langue de l'application correspond à celle du navigateur.
+        expect(i18nextMock.lng).toBe(navigator.language.split("-")[0]);
+
+        // When : Le navigateur change de langue (ex: espagnol)
+        i18nextMock.changeLanguage("es");
+
+        // Then : L'application applique la nouvelle langue
+        expect(i18nextMock.lng).toBe("es");
+    });
 });
 
 test("translation_ChangeNavigatorLanguage_ThrowException", () => {
