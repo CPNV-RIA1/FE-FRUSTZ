c70473bba80f43a13224652f91ce6a6246a9091f
Julien Schneider <julienschneider964@gmail.com>
2025-03-11 20:38:30 +0100

    test: add JEST test for scenario 4

--- test/jest/translation.test.js
+++ test/jest/translation.test.js
@@ -1,4 +1,5 @@
 "use strict";
+const LanguageUnknowException = require("../../app/exceptions/LanguageUnknowException");
 
 describe("Scenario 001 - Langue par défaut", () => {
     test("translation_NominalLanguage_NavigatorLanguageKnow", () => {
@@ -36,9 +37,15 @@ describe("Scenario 003 - Changement de langue du navigateur - exception", () => 
 });
 
 describe("Scenario 004 - Traduction de la page", () => {
-    test("translation_ChangeNavigatorLanguage_PageIsTranslated", () => {
-        //given
-        //when
-        //then
+    test("translation_ExterneTranslatorLanguage_WordUntranslate", () => {
+        // Given :
+        // La page d'accueil est chargée.
+        // La langue de l'application correspond à celle du navigateur.
+        expect(i18nextMock.lng).toBe(navigator.language.split("-")[0]);
+        
+        //When : Activation d'un outil de traduction (navigateur). Peut-importe la langue.
+        simulateTranslation(elements);
+        //Then : Les noms communs, les noms de marques, les produits ne sont pas traduits
+        expect(document.querySelector("p[translate='no']").textContent).toBe("Couteaux Suisses");
     });
 });
