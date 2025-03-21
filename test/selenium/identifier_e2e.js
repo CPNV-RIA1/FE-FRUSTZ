0a3daa71810db12594d3f468f54233286af57dff
Julien Schneider <julienschneider964@gmail.com>
2025-03-21 10:22:10 +0100

    test: scenario 1 add default language navigator

--- test/selenium/identifier_e2e.js
+++ test/selenium/identifier_e2e.js
@@ -3,6 +3,11 @@ const { Builder, By, Key, until } = require("selenium-webdriver");
 const APP_URI = "http://127.0.0.1:3000/";
 
 async function identifier_LoadingPage_NavigatorLoadIdentifierPage() {
+    let options = new (require("selenium-webdriver/chrome").Options)();
+    options.setUserPreferences({
+        "intl.accept_languages": "en",
+    });
+    
     let driver = await new Builder().forBrowser("chrome").build();
 
     try {
@@ -19,9 +24,10 @@ async function identifier_LoadingPage_NavigatorLoadIdentifierPage() {
 
         // Then: L'application a pu naviguer jusqu'à la page de login.
         await driver.sleep(2000);
-        let pageTitle = await driver.getTitle();
+        let pageTitle = await driver.findElement(By.id("page-title"));
+        let textPageTitle = await pageTitle.getText();
 
-        if (pageTitle.includes("Authentification")) {
+        if (textPageTitle == "Get started today!") {
             console.log("✅ La bonne page a été chargée !");
         } else {
             console.log("❌ Mauvaise page chargée !");
