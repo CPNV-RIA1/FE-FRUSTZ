6a72ef70c0edb75b5ee6d10bdfdfb59296257163
Julien Schneider <julienschneider964@gmail.com>
2025-03-21 10:51:14 +0100

    test: add test for scenario 2, possibility to change language

--- test/selenium/identifier_e2e.js
+++ test/selenium/identifier_e2e.js
@@ -1,6 +1,7 @@
 const { Builder, By, Key, until } = require("selenium-webdriver");
 
 const APP_URI = "http://127.0.0.1:3000/";
+const APP_IDENTIFIER_URI = "http://127.0.0.1:3000/app/views/identifier.html";
 
 async function identifier_LoadingPage_NavigatorLoadIdentifierPage() {
     let options = new (require("selenium-webdriver/chrome").Options)();
@@ -40,8 +41,56 @@ async function identifier_LoadingPage_NavigatorLoadIdentifierPage() {
     }
 }
 
+async function identifier_NominalLanguage_ChangeNavigatorLanguage() {
+    let options = new (require("selenium-webdriver/chrome").Options)();
+    options.setUserPreferences({
+        "intl.accept_languages": "en",
+    });
+
+    let driver = await new Builder().forBrowser("chrome").build();
+
+    try {
+        // Given: 
+        // La page de login est affichée.
+        await driver.get(APP_IDENTIFIER_URI);
+        await driver.sleep(2000);
+
+        let defaultNavigatorLng = await driver.executeScript(
+            "return navigator.language || navigator.userLanguage;"
+        );
+
+        // When: Je débute la saisie...
+
+        // Then: la possiblité de modifier la langue (FR/ALL/ANG).
+        let selectElement = await driver.findElement(By.id("languageSelector"));
+        await selectElement.click();
+        await driver.sleep(2000);
+        await driver.findElement(By.css('option[value="fr"]')).click();
+
+        await driver.sleep(2000);
+
+        let newLang = await driver
+            .findElement(By.id("languageSelector"))
+            .getAttribute("value");
+
+        console.log(`Nouvelle langue après sélection : ${newLang}`);
+
+        if (newLang !== defaultNavigatorLng) {
+            console.log("✅ Test réussi : la langue a bien été changée.");
+        } else {
+            console.log("❌ Test échoué : la langue ne s'est pas mise à jour.");
+        }
+
+    } catch (error) {
+        console.error("Erreur :", error);
+    } finally {
+        await driver.quit();
+    }
+}
+
 
 
 (async function runTests() {
-    await identifier_LoadingPage_NavigatorLoadIdentifierPage();
+    //await identifier_LoadingPage_NavigatorLoadIdentifierPage();
+    await identifier_NominalLanguage_ChangeNavigatorLanguage();
 })();
