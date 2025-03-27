395f591ddfa3387778e892c6f7cf8850da3807ce
dieperid <david.dieperink@ik.me>
2025-03-27 13:27:02 +0100

    refactor: deleted main.js for running selenium tests

--- test/selenium/main.js
+++
@@ -1,9 +0,0 @@
-const path = require("path");
-const fs = require("fs");
-
-fs.readdirSync(__dirname).forEach((file) => {
-    if (file.endsWith(".js") && file !== "main.js") {
-        console.log(`Running test: ${file}`);
-        require(path.join(__dirname, file));
-    }
-});
